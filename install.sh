3bb6efecabfd4c06ee1019dc6e213d7fb2df2d5d
thehaniak <thehaniak@users.noreply.github.com>
2025-09-21 15:08:17 +0100

    more cleanup

--- install.sh
+++ install.sh
@@ -10,75 +10,6 @@ readonly ARCHITECTURE=$(arch)
 readonly FEDORA_SUPPORTED_VERSIONS="41 42"
 
 
-add_upstart_script()
-{
-  cat > /etc/init/smiusbdisplay.conf <<'EOF'
-description "SiliconMotion Driver Service"
-
-
-start on login-session-start
-stop on desktop-shutdown
-
-# Restart if process crashes
-respawn
-
-# Only attempt to respawn 10 times in 5 seconds
-respawn limit 10 5
-
-chdir /opt/siliconmotion
-
-pre-start script
-    . /opt/siliconmotion/smi-udev.sh
-
-    if [ "\$(get_siliconmotion_dev_count)" = "0" ]; then
-        stop
-        exit 0
-    fi
-end script
-script
-    [ -r /etc/default/siliconmotion ] && . /etc/default/siliconmotion
-    modprobe evdi
-    if [ $? != 0 ]; then
-	local v=$(awk -F '=' '/PACKAGE_VERSION/{print $2}' /opt/siliconmotion/module/dkms.conf)
-	dkms remove -m evdi -v $v --all
-	if [ $? != 0 ]; then
-    		rm –rf /var/lib/dkms/$v
-	fi
-	dkms install /opt/siliconmotion/module/
-	if [ $? == 0 ]; then
-		cp /opt/siliconmotion/evdi.conf /etc/modprobe.d
-		modprobe evdi
-	fi
-    fi
-    exec LD_LIBRARY_PATH=/opt/siliconmotion /opt/siliconmotion/SMIUSBDisplayManager
-end script
-EOF
-
-  chmod 0644 /etc/init/smiusbdisplay.conf
-}
-
-remove_upstart_script()
-{
-  rm -f /etc/init/smiusbdisplay.conf
-}
-
-add_wayland_script()
-{
-if [ "$(lsb_release -r --short)"  == "20.04" ];
-then
-  mkdir -p /usr/share/xsessions/hidden
-  dpkg-divert --rename --divert /usr/share/xsessions/hidden/ubuntu.desktop --add /usr/share/xsessions/ubuntu.desktop
-fi
-}
-
-remove_wayland_script()
-{
-if [ "$(lsb_release -r --short)"  == "20.04" ];
-then
-  dpkg-divert --rename --remove /usr/share/xsessions/ubuntu.desktop
-fi
-}
-
 add_systemd_service()
 {
   cat > /lib/systemd/system/smiusbdisplay.service <<'EOF'
@@ -106,35 +37,6 @@ trigger_udev_if_devices_connected()
   for device in $(grep -lw 090c /sys/bus/usb/devices/*/idVendor); do
     udevadm trigger --action=add "$(dirname "$device")"
   done
-}
-
-remove_systemd_service()
-{
-  driver_name="smiusbdisplay"
-  echo "Stopping ${driver_name} systemd service"
-  systemctl stop ${driver_name}.service
-  systemctl disable ${driver_name}.service
-  rm -f /lib/systemd/system/${driver_name}.service
-}
-
-remove_pm_scripts()
-{
-  rm -f /etc/pm/sleep.d/smipm.sh
-  rm -f /lib/systemd/system-sleep/smipm.sh
-}
-
-cleanup()
-{
-  rm -rf $COREDIR
-  rm -rf $LOGPATH
-  rm -f /usr/bin/smi-installer
-  rm -f /usr/bin/SMIFWLogCapture
-}
-
-binary_location()
-{
-  echo "$(pwd)/binaries"
-
 }
 
 install()
@@ -177,8 +79,6 @@ install()
   siliconmotion_bootstrap_script="$COREDIR/smi-udev.sh"
   create_bootstrap_file "$SYSTEMINITDAEMON" "$siliconmotion_bootstrap_script"
 
-  add_wayland_script
-
   echo "Adding udev rule for SiliconMotion devices"
   create_udev_rules_file /etc/udev/rules.d/99-smiusbdisplay.rules
 
@@ -197,54 +97,6 @@ install()
   echo -e "\nPlease reboot your computer and check if everything is working as intended."
 }
 
-## uninstall option is not properly tested yet, so it is disabled for now.
-uninstall()
-{
-  echo "Uninstall is not implemented yet."
-  exit 0
-
-  if [ "upstart" == "$SYSTEMINITDAEMON" ]; then
-    echo "Stopping SMIUSBDisplay upstart job"
-    stop smiusbdisplay
-    remove_upstart_script
-  elif [ "systemd" == "$SYSTEMINITDAEMON" ]; then
-    echo "Stopping SMIUSBDisplay systemd service"
-    systemctl stop smiusbdisplay.service
-    remove_systemd_service
-
-  fi
-
-  echo "[ Removing suspend-resume hooks ]"
-  #remove_pm_scripts
-
-  echo "[ Removing udev rule ]"
-  rm -f /etc/udev/rules.d/99-smiusbdisplay.rules
-  udevadm control -R
-  udevadm trigger
-
-  remove_wayland_script
-
-  echo "[ Removing Core folder ]"
-  cleanup
-
-  modprobe -r evdi
-
-  if [ -d $OTHERPDDIR ]; then
-	  echo "WARNING: There are other products in the system using EVDI."
-  else
-	  echo "Removing EVDI from kernel tree, DKMS, and removing sources."
-    	  (
-    	  cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" && \
-	  uninstall_evdi_module "evdi.tar.gz"
-    	  )
-  fi
-
-  echo -e "\nUninstallation steps complete."
-  if [ -f /sys/devices/evdi/count ]; then
-    echo "Please note that the evdi kernel module is still in the memory."
-    echo "A reboot is required to fully complete the uninstallation process."
-  fi
-}
 
 usage()
 {
