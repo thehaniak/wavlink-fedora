84634275c7b54da04328f566c01770c3045103b7
thehaniak <thehaniak@users.noreply.github.com>
2025-09-21 14:08:17 +0100

    Improved install.sh code

--- install.sh
+++ install.sh
@@ -197,6 +197,7 @@ install()
   echo -e "\nPlease reboot your computer and check if everything is working as intended."
 }
 
+## uninstall option is not properly tested yet, so it is disabled for now.
 uninstall()
 {
   echo "Uninstall is not implemented yet."
@@ -249,7 +250,8 @@ usage()
 {
   echo
   echo "Installs $PRODUCT."
-  echo "Usage: $0 [ install | uninstall | help | system-check ]"
+  echo "Usage:"
+  echo " sudo $0 [ install | help | system-check ]"
   echo
   echo "The default operation is system-check."
   echo "If unknown argument is given, a quick compatibility check is performed but nothing is installed."
@@ -258,17 +260,16 @@ usage()
 detect_init_daemon()
 {
     INIT=$(readlink /proc/1/exe)
-    if [ "$INIT" == "/sbin/init" ]; then
+    if [ "$INIT" == "/sbin/init" ]
+    then
         INIT=$(/sbin/init --version)
     fi
 
-    [ -z "${INIT##*upstart*}" ] && SYSTEMINITDAEMON="upstart"
+
     [ -z "${INIT##*systemd*}" ] && SYSTEMINITDAEMON="systemd"
 
     if [ -z "$SYSTEMINITDAEMON" ]; then
-        echo "ERROR: the installer script is unable to find out how to start SMIUSBDisplayManager service automatically on your system." >&2
-        echo "Please set an environment variable SYSTEMINITDAEMON to 'upstart' or 'systemd' before running the installation script to force one of the options." >&2
-        echo "Installation terminated." >&2
+        echo "Error: You're not using the upstart system. This is no longer supported. Please use systemd." >&2
         exit 1
     fi
 }
@@ -285,32 +286,32 @@ prompt_supported()
 
 system_check()
 {
-  echo -n "System check: "
+  echo "System check:"
 
   if [ -f /etc/fedora-release ] \
     && [[ "${ARCHITECTURE}" == "x86_64" ]] \
     && [[ "${FEDORA_SUPPORTED_VERSIONS}" =~ (" "|^)${FEDORA_VERSION}(" "|$) ]]
   then
-    echo "Found Fedora ${FEDORA_VERSION} on arch ${ARCHITECTURE}."
+    echo "✔ Found Fedora ${FEDORA_VERSION} on arch ${ARCHITECTURE}"
+    echo "✔ Found init system: $SYSTEMINITDAEMON"
     return 0
-  else
-    echo "This script has not been tested on your distro/release or arquitecture."
   fi
 
+  echo "⚠ This script has not been tested on your distro/release or arquitecture:"
+  echo "  - Supported Fedora versions are: ${FEDORA_SUPPORTED_VERSIONS}"
+  echo "  - Supported architecture is ${ARCHITECTURE}"
   return 1
 }
 
 
-if [ "$(id -u)" != "0" ]; then
-  echo "You need to be root to use this script." >&2
-  exit 1
-fi
-
-[ -z "$SYSTEMINITDAEMON" ] && detect_init_daemon || echo "Trying to use the forced init system: $SYSTEMINITDAEMON"
-
 case $1 in
 
   "install")
+    if [ "$(id -u)" != "0" ]; then
+      echo "You need to be root to use this script." >&2
+      exit 1
+    fi
+    detect_init_daemon
     prompt_supported
     install
     ;;
@@ -319,12 +320,8 @@ case $1 in
     usage
     ;;
 
-  "remove")
-    echo "Remove not implemented."
-    ;;
-
   *)
-    if system_check
+    if system_check && detect_init_daemon
     then
       echo "To install run \"sudo $0 install\"."
     fi
